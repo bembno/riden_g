930c69c30e8581963c7b25d6969d934d931a9124
Paw <bembnowiczp@gmail.com>
2025-10-10 16:19:53 +0200

    before mqqt

--- storage/ridsoy.py
+++ storage/ridsoy.py
@@ -1,8 +1,17 @@
 from drivers.riden import Riden
+from drivers.InverterController import InverterController
+import serial, time
 
 # These are the default values for port, baudrate, and address
 charger = Riden(port="/dev/ttyUSB0", baudrate=115200, address=1)
 
+
+inverter = InverterController()
+inverter.Connect()
+inverter.ThreadLooping(start_power=0)
+time.sleep(5)
+inverter.ModifyPower(120)
+time.sleep(10)
 # Getters and Setters are available
 charger.set_v_set(57)
 charger.set_i_set(0.69)
